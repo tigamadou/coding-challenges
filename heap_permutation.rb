ec46973ff2eb4a1d9e2027e1883f08e6c3ccb0e5
Amadou <amadou140589@gmail.com>
2021-09-09 09:32:50 +0100

    heap_permutation added

---
+++ heap_permutation.rb
@@ -0,0 +1,15 @@
+def heap_permutation(data, n)
+    if n == 1
+        print data
+        return
+    end
+    
+    n.times do |i|
+        heap_permutation(data, n - 1)
+        if n % 2 == 0
+            data[i], data[n-1] = data[n-1], data[i]
+        else
+            data[0], data[n-1] = data[n-1], data[0]
+        end
+    end
+end
